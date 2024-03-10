da0522ddd2bc21c75a7b34e5fa833abf4763f265
namskynet <158797778+namskynet@users.noreply.github.com>
2024-03-10 16:17:06 +1100

    a
    a

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
-let classLink = '.menu-page1'
+let classLink = '.menupage'
 
 window.onscroll = function() {
     let h = document.documentElement.clientHeight;
 
     if (window.scrollY >=h*4) {
-        classLink = '.menu-page5'
+        classLink = '.mainpage';
     }
     else if (window.scrollY >=h*3) {
-        classLink = '.menu-page4'
+        classLink = '.aboutme';
     }
     else if (window.scrollY >=h*2) {
-        classLink = '.menu-page3'
+        classLink = '.skills';
     }
     else if (window.scrollY >=h) {
-        classLink = '.menu-page2'
+        classLink = '.projects';
     }
     else {
-        classLink = '.menu-page1'
+        classLink = '.comments';
     }
 
-    let sec = document.querySelectorAll()
-    
-    let activeBTN = document.querySelector('.active');
-    let newactiveBTN = document.querySelector(classLink);
-    
-    if (!newactiveBTN.classList.contains('.active')) {
-        newactiveBTN.classList.add('.active')
-        activeBTN.classList.remove(('.active'))
-    }
 
 };
 
+let activeBTN = document.querySelector('.active');
+let newactiveBTN = document.querySelector(classLink);
+
+if (!newactiveBTN.classList.contains('active')) {
+    newactiveBTN.classList.add('active')
+    activeBTN.classList.remove('active')
+}
+
+
+
+// let sec = document.querySelectorAll()
 // let section = document.querySelectorAll('section')
 // let navlinks = document.querySelectorAll('header menupage')
 
